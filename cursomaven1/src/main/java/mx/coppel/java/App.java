316239d07039db9e4e3332562ab66ed309e6c8bd
capacitacion-ic <53872495+capacitacion-ic@users.noreply.github.com>
2019-08-07 17:12:05 -0600

    Update App.java

--- cursomaven1/src/main/java/mx/coppel/java/App.java
+++ cursomaven1/src/main/java/mx/coppel/java/App.java
@@ -8,27 +8,25 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class App 
 {
-	private static String FILE = "C:\\JavaArchitect\\example.pdf";
+    private static String FILE = "C:\\JavaArchitect\\example.pdf";
     
     public static void main( String[] args )
     {        
         try {
-
-        	System.out.println( "Inicio Generar PDF" );
-			
-        	Document document = new Document();
-            
-            PdfWriter.getInstance(document, new FileOutputStream(FILE));
-            document.open();            
-            
-            Paragraph paragraph = new Paragraph("TEXTO EXPORTADO A PDF");
-            document.add(paragraph); 
-            
-            document.close();
-            
-            System.out.println( "Fin Generar PDF" );
+		System.out.println( "Inicio Generar PDF" );
+		
+		Document document = new Document();
+		PdfWriter.getInstance(document, new FileOutputStream(FILE));
+		document.open();
+		
+		Paragraph paragraph = new Paragraph("TEXTO EXPORTADO A PDF");
+		document.add(paragraph);
+		
+		document.close();
+		
+		System.out.println( "Fin Generar PDF" );
         } catch (Exception e) {
-            e.printStackTrace();
+		e.printStackTrace();
         }        
     }     
 }
